d881f3856e086ec388aab1fee7a644a79ca3128d
Elvis Melkić <elvis.melkic@gmail.com>
2020-11-02 21:03:47 +0100

    Add conditional routing

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -4,11 +4,16 @@ import { TARGET_CLASS } from '../../utils/visible'
 
 import './index.scss'
 
-export const ThumbnailItem = ({ node }) => (
-  <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
-    <div key={node.fields.slug}>
-      <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <p>{node.frontmatter.summary}</p>
-    </div>
-  </Link>
-)
+export const ThumbnailItem = ({ node }) => {
+  const outsideRouteTitle = 'How to build a self-healing system using supervision tree in Elixir'
+  const route = node.frontmatter.title === outsideRouteTitle ? 'https://kodius.com/blog/elixir-supervision-tree' : node.fields.slug
+
+  return (
+    <Link className={`thumbnail ${TARGET_CLASS}`} to={route}>
+      <div key={node.fields.slug}>
+        <h3>{node.frontmatter.title || node.fields.slug}</h3>
+        <p>{node.frontmatter.summary}</p>
+      </div>
+    </Link>
+  )
+}
